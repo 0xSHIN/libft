9166c24cc267c579ce424b3bb828e2a2fe458d26
shindroid <alexyildiz@MacBook-Air-de-Alex.local>
2023-05-03 20:14:50 +0200

    fixed

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -5,67 +5,28 @@ t_list *ft_lstlast(t_list *lst)
 	int i;
 
 	i = 0;
-	while (lst)
+	while (lst->next != NULL)
 	{
 		i++;
-		lst->next;
+		lst = lst->next;
 	}
-	return (lst);
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-
-t_list *ft_lstnew(void *content)
-{
-    t_list *new_node;
-
-    new_node = (t_list *)malloc(sizeof(t_list));
-    if (new_node == NULL)
-    {
-        return NULL;
-    }
-
-    new_node->content = content;
-    new_node->next = NULL;
-
-    return new_node;
+	return (lst->content);
 }
 
 int main(void)
 {
-    t_list *head = NULL;
-    t_list *last = NULL;
-
-    t_list *node1 = ft_lstnew("Node 1");
-    t_list *node2 = ft_lstnew("Node 2");
-    t_list *node3 = ft_lstnew("Node 3");
-
-    if (node1 == NULL || node2 == NULL || node3 == NULL)
-    {
-        printf("Failed to allocate memory for nodes.\n");
-        return 1;
-    }
-
-    ft_lstadd_front(&head, node1);
-    ft_lstadd_front(&head, node2);
-    ft_lstadd_front(&head, node3);
-
-    last = ft_lstlast(head);
-
-    if (last != NULL)
-    {
-        printf("The content of the last node is: %s\n", (char *)last->content);
-    }
-    else
-    {
-        printf("The list is empty.\n");
-    }
-
-    // Free the allocated memory for nodes
-    free(node1);
-    free(node2);
-    free(node3);
-
-    return 0;
+		t_list *list_test;
+		int i = 3;
+		t_list *first;
+		list_test = malloc(sizeof(t_list));
+		first = list_test;
+		while (i)
+		{
+				list_test->next= malloc(sizeof(t_list));
+				list_test = list_test->next;
+				i--;
+		}
+		list_test = first;
+		printf("taille = %d \n", ft_lstlast(list_test));
+		return (0);
 }
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -1,3 +1,4 @@
+#include "./libft.h"
 int ft_lstsize(t_list *lst)
 {
 	int	i;
@@ -9,4 +10,22 @@ int ft_lstsize(t_list *lst)
 		i++; 
 	}
 	return (i);
+}
+
+int main(void)
+{
+		t_list *list_test;
+		int i = 3;
+		t_list *first;
+		list_test = malloc(sizeof(t_list));
+		first = list_test;
+		while (i)
+		{
+				list_test->next= malloc(sizeof(t_list));
+				list_test = list_test->next;
+				i--;
+		}
+		list_test = first;
+		printf("taille = %d \n", ft_lstsize(list_test));
+		return (0);
 }
